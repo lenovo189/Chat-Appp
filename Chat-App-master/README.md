814638f07f8db11e2cd14a4e528b2491b280c1dd
lenovo189 <156093567+lenovo189@users.noreply.github.com>
2024-12-20 21:12:03 +0500

    Add files via upload

---
+++ Chat-App-master/README.md
@@ -0,0 +1,6 @@
+# Chat-App
+Chat App using HTML, CSS, JavaScript, Bootstrap &amp; Firebase
+
+App Link :-
+
+https://chatapp-91755.firebaseapp.com/
---
+++ Chat-App-master/js/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmxdPhG2cqqh2MZhuX9_MQP3D5FZ4zSYk",
+    authDomain: "login-with-firebase-data-8bc55.firebaseapp.com",
+    databaseURL: "https://login-with-firebase-data-8bc55-default-rtdb.firebaseio.com",
+    projectId: "login-with-firebase-data-8bc55",
+    storageBucket: "login-with-firebase-data-8bc55.firebasestorage.app",
+    messagingSenderId: "630971128758",
+    appId: "1:630971128758:web:55f7a280437dd9133dbf17"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  console.log("Successfully Connected to Firebase !!!");
